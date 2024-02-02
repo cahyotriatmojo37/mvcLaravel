7ce7855c102fdd306eac45e11080b9d3e6f82b9d
cahyotriatmojo <111202113617@mhs.dinus.ac.id>
2024-02-03 00:36:23 +0700

    first commit

---
+++ resources/views/header.blade.php
@@ -0,0 +1,16 @@
+<header>
+<ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="<?php echo url('daftar')?>">Link  Daftar</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#">ComingSoon</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+  </li>
+</ul>
+</header>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('main')
+@section('content')
+
+<div class="container mt-2">
+   <h3>Form Regis</h3>
+   <p>Tolong Di isi</p>
+</div>
+
+@endsection
